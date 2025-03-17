f78a851e2e4c2499fae65ffa0c06d1109a705c5a
Ahmed-Elimam <aa.emam97@gmail.com>
2025-03-17 22:42:02 +0200

    add admin seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('123456'),
+        ]);
+    }
+}
